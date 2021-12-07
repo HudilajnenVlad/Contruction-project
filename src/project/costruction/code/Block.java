0eb3a5192c0247ea52c2248253f18c357f0d32ea
ivan-gorshechnikov <vania.gorshevhnikov@gmail.com>
2021-12-07 22:19:02 +0300

    Init project and make simple gameplay logic

---
+++ src/project/costruction/code/Block.java
@@ -0,0 +1,7 @@
+package project.costruction.code;
+
+import java.awt.*;
+
+public class Block {
+
+}
---
+++ src/project/costruction/code/Figure.java
@@ -0,0 +1,34 @@
+package project.costruction.code;
+
+public class Figure {
+    void drop()
+    {
+
+    }
+    void rotate ()
+    {
+
+    }
+
+    void move (int a)
+    {
+
+    }
+
+    boolean isTouchGround()
+    {
+return true;
+    }
+    void leaveOnGround()
+    {
+
+    }
+    boolean isCrossGround()
+    {
+        return true;
+    }
+    void stepDown()
+    {
+
+    }
+}
