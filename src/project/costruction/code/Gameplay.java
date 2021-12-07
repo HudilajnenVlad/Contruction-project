84da9d5541b35c6914c7484b31b77845eabec534
ivan-gorshechnikov <vania.gorshevhnikov@gmail.com>
2021-12-08 00:06:49 +0300

    Make gameplay logic

--- src/project/costruction/code/Gameplay.java
+++ src/project/costruction/code/Gameplay.java
@@ -7,22 +7,22 @@ import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Gameplay {
+public class Gameplay extends JFrame{
 
     final String TITLE_OF_PROGRAM = "Tetris"; //название окна
     static final int BLOCK_SIZE = 25; //размер одного блока
     static final int ARC_RADIUS = 6; //закругление краев
-    final int FIELD_WIDTH = 10; //количество блоков в ширину
-    final int FIELD_HEIGHT = 18; // в высоту
-    final int START_LOCATION = 180; // начальное положение
-    final int FIELD_DX = 7; // смещение для нормального размера окна в ширину
-    final int FIELD_DY = 26; // смещение для нормального размера окна в высоту
-    final int LEFT = 37; //кодировки кнопки влево
-    final int UP = 38; //вверх
-    final int RIGHT = 39; // вправо
-    final int DOWN = 40; // вниз
-    final int SHOW_DELAY = 350; // задержка хода
-    final int[][][] SHAPES = { //массив всех фигур
+    static final int FIELD_WIDTH = 10; //количество блоков в ширину
+    static final int FIELD_HEIGHT = 18; // в высоту
+    static final int START_LOCATION = 180; // начальное положение
+    static final int FIELD_DX = 7; // смещение для нормального размера окна в ширину
+    static final int FIELD_DY = 26; // смещение для нормального размера окна в высоту
+    static final int LEFT = 37; //кодировки кнопки влево
+    static final int UP = 38; //вверх
+    static final int RIGHT = 39; // вправо
+    static final int DOWN = 40; // вниз
+    static final int SHOW_DELAY = 350; // задержка хода
+    static final int[][][] SHAPES = { //массив всех фигур
             {{0, 0, 0, 0}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {4, 0x00f0f0}}, // I
             {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {4, 0xf0f000}}, // O
             {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {3, 0x0000f0}}, // J
@@ -33,13 +33,12 @@ public class Gameplay {
     };
     final int[] SCORES = {100, 300, 700, 1500}; // счета за закрытия строки
     int gameScore = 0;
-    int[][] mine = new int[FIELD_HEIGHT + 1][FIELD_WIDTH]; // поле длля игры
+    static int[][] mine = new int[FIELD_HEIGHT + 1][FIELD_WIDTH]; // поле длля игры
     JFrame frame; //наши окна
     Canvas canvas = new Canvas();
-    Random random = new Random();
-    Figure figure = new Figure();
-    boolean gameOver = false;
-    final int[][] GAME_OVER_MSG = { //код для конча игры
+    static Figure figure = new Figure();
+    static boolean gameOver = false;
+    static final int[][] GAME_OVER_MSG = { //код для конца игры
             {0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
             {1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
             {1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1},
@@ -52,53 +51,59 @@ public class Gameplay {
             {1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0},
             {1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0},
             {0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0}};
-
-    void run() {
-        frame = new JFrame(TITLE_OF_PROGRAM); //создаем окно
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // добавляем обработчик закрытия окна
-        frame.setSize(FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY); // задаем размеры окна
-        frame.setLocation(START_LOCATION, START_LOCATION); // задаем первоначальное положение окна
-        frame.setResizable(false); // делаем окно константного размера
-        canvas.setBackground(Color.black); // задаем задний цвет
-        frame.addKeyListener(new KeyAdapter() { // задаем обработчик нажатий клавиш
-            @Override
+    public Gameplay() {
+        setTitle(TITLE_OF_PROGRAM); //задаем название окна
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // задаем для закрытия
+        setBounds(START_LOCATION, START_LOCATION, FIELD_WIDTH * BLOCK_SIZE + FIELD_DX, FIELD_HEIGHT * BLOCK_SIZE + FIELD_DY);
+        setResizable(false); // делаем окно неизменяемым
+        canvas.setBackground(Color.black); // задаем цвет задника
+        addKeyListener(new KeyAdapter() { //обработчик нажатий клавиш
             public void keyPressed(KeyEvent e) {
                 if (!gameOver) {
-                    switch (e.getKeyCode()) {
-                        case DOWN -> figure.drop();//опускаем сразу вниз
-                        case UP -> figure.rotate();//делаем поворот по часовой
-                        case LEFT -> figure.move(LEFT); //смещаем влево
-                        case RIGHT -> figure.move(RIGHT); //или вправо
-                        default -> {
-                        }
-                    }
-                    canvas.repaint();
+                    if (e.getKeyCode() == DOWN) figure.drop();
+                    if (e.getKeyCode() == UP) figure.rotate();
+                    if (e.getKeyCode() == LEFT || e.getKeyCode() == RIGHT) figure.move(e.getKeyCode());
                 }
+                canvas.repaint();
             }
         });
-        frame.setVisible(true); //показываем наше окно
+        add(BorderLayout.CENTER, canvas);
+        setVisible(true);
+        Arrays.fill(mine[FIELD_HEIGHT], 1); // create a ground for mines
+    }
 
-        Arrays.fill(mine[FIELD_HEIGHT], 1); //инициализурем поле
-
-        while (!gameOver) { //идем пока игра не закончилась
+    void run() { // main loop of game
+        while (!gameOver) {
             try {
-                Thread.sleep(SHOW_DELAY); //делаем задрежку
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+                Thread.sleep(SHOW_DELAY);
+            } catch (Exception e) { e.printStackTrace(); }
             canvas.repaint();
-            if (figure.isTouchGround()) { // проверяем дошли ли до конца
-                figure.leaveOnGround(); // вставляем фигуру вниз
-                checkFilling(); // проверяем на заполнение строк
-                figure = new Figure(); // создаем новую фигуру
-                gameOver = figure.isCrossGround(); // проверяем на конец игры
-            } else {
-                figure.stepDown(); //опускаем фигуру на один вниз, если еще не дошла до конца
-            }
+            checkFilling();
+            if (figure.isTouchGround()) {
+                figure.leaveOnTheGround();
+                figure = new Figure();
+                gameOver = figure.isCrossGround(); // Is there space for a new figure?
+            } else
+                figure.stepDown();
         }
     }
 
-    void checkFilling() {
-
+    void checkFilling() { // check filling rows
+        int row = FIELD_HEIGHT - 1;
+        int countFillRows = 0;
+        while (row > 0) {
+            int filled = 1;
+            for (int col = 0; col < FIELD_WIDTH; col++)
+                filled *= Integer.signum(mine[row][col]);
+            if (filled > 0) {
+                countFillRows++;
+                for (int i = row; i > 0; i--) System.arraycopy(mine[i-1], 0, mine[i], 0, FIELD_WIDTH);
+            } else
+                row--;
+        }
+        if (countFillRows > 0) {
+            gameScore += SCORES[countFillRows - 1];
+            setTitle(TITLE_OF_PROGRAM + " : " + gameScore);
+        }
     }
 }
