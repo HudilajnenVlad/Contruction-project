93d3361b4bbf32d898a08ce77455366dc102a2b3
Kristina-Kljaich <kriatinakooper1@gmail.com>
2021-12-10 22:28:24 +0300

    Add visual of figures and blocks

--- src/project/costruction/code/Block.java
+++ src/project/costruction/code/Block.java
@@ -3,5 +3,31 @@ package project.costruction.code;
 import java.awt.*;
 
 public class Block {
+    private int x, y;
 
+    public Block(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    void paint(Graphics g, int color) {
+        g.setColor(new Color(color));
+        g.drawRoundRect(x * Gameplay.BLOCK_SIZE + 1, y * Gameplay.BLOCK_SIZE + 1, Gameplay.BLOCK_SIZE - 2, Gameplay.BLOCK_SIZE - 2, Gameplay.ARC_RADIUS, Gameplay.ARC_RADIUS);
+    }
 }
